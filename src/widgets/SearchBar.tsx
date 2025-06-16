b2241f74b49300a7ae7af540a5522701846e529f
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-06-16 18:54:07 -0300

    feat: adicionando opções de destino, datas e hóspedes na barra de pesquisa

--- src/widgets/SearchBar.tsx
+++ src/widgets/SearchBar.tsx
@@ -4,7 +4,30 @@ import { IconSearch } from "@tabler/icons-react";
 export default function SearchBar() {
   return (
     <div className="w-full max-w-2xl mx-auto border-1 border-gray-200 flex items-center rounded-full overflow-clip px-4 py-2 shadow-lg shadow-gray-200">
-      <input className="w-full text-lg focus:outline-none px-3" type="text" placeholder="Buscar destinos" />
+      
+      <div className="flex w-full justify-around">
+        
+        <div className="flex flex-col items-start">
+          <label className="text-sm font-semibold">Onde</label>
+          <input className="w-32 text-sm focus:outline-none border-r-1 border-gray-300" type="text" placeholder="Buscar destinos" />
+        </div>
+
+        <div className="flex flex-col items-start">
+          <label className="text-sm font-semibold">Check-in</label>
+          <input className="w-32 text-sm focus:outline-none border-r-1 border-gray-300" type="text" placeholder="Insira as datas" />
+        </div>
+        
+        <div className="flex flex-col items-start">
+          <label className="text-sm font-semibold">Checkout</label>
+          <input className="w-32 text-sm focus:outline-none border-r-1 border-gray-300" type="text" placeholder="Insira as datas" />
+        </div>
+        
+        <div className="flex flex-col items-start">
+          <label className="text-sm font-semibold">Quem</label>
+          <input className="w-20 text-sm focus:outline-none" type="text" placeholder="Hóspedes" />
+        </div>
+      </div>
+
       <button className="bg-red-500 rounded-full p-2 cursor-pointer hover:bg-red-600">
         <IconSearch 
           aria-label="Ícone de busca"
